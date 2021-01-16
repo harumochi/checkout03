64c34187f51f0f46a85ccd61a62c1909cfab0ac4
haru mo <harunori@rosmarin-y.co.jp>
2021-01-16 21:28:30 +0000

    NoOfPVs_CurPC Final ver

--- models/NoOfPVs_CurPC.sql
+++ models/NoOfPVs_CurPC.sql
@@ -1,13 +1,14 @@
-WITH Cur_PC as (select id, postcode, max(pageview_datetime) as date_time
-from users_extract01.postcode_history
-where (postcode is not null and pageview_datetime is not null)
-group by id, postcode)
-select CPC.id, CPC.postcode, Count(PCH.pageview_datetime) as NoOfPVs
-from Cur_PC as CPC
-left join 
-(select*from users_extract01.postcode_history
-where 
-DATE(pageview_datetime) >= DATE_SUB(CURRENT_DATE, INTERVAL 180 DAY)
-) as PCH
+With CPC as (
+        With Cur_PC_id as (select id, max(pageview_datetime) as max_date_time
+        from users_extract01.postcode_history
+        group by id)
+        select a.id, b.postcode, max_date_time from 
+        Cur_PC_id as a
+        Left join users_extract01.postcode_history as b
+        on a.max_date_time =b.pageview_datetime and a.id = b.id
+) 
+select CPC.postcode, Count(PCH.pageview_datetime) as NoOfPVs
+from CPC
+left join users_extract01.postcode_history as PCH
 on PCH.id=CPC.id and PCH.postcode=CPC.postcode
-Group by CPC.id, CPC.postcode
+Group by CPC.postcode
